259853a6637cf3e5708c75f78afc1dd6e575e5d2
Timon <timon.hueppi@gmail.com>
2021-04-27 13:13:22 +0200

    Adds 3.1

---
+++ 3.1_Formular/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("btnSubmit").addEventListener("click", (oEvent) => {
+        //oEvent.preventDefault();
+    });
+});
