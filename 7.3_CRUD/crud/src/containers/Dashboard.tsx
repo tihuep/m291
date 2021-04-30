bc44f513e261150034daa6dea9ac0690a550dad7
tihuep <timon.hueppi@gmail.com>
2021-04-30 19:03:33 +0200

    Updates 7.3

--- 7.3_CRUD/crud/src/containers/Dashboard.tsx
+++ 7.3_CRUD/crud/src/containers/Dashboard.tsx
@@ -1,6 +1,12 @@
 import { NavLink } from "react-router-dom";
 
 function Dashboard() {
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log("Latitude is :", position.coords.latitude);
+        console.log("Longitude is :", position.coords.longitude);
+    });
+
     return (
     <div>dashboard
         <NavLink to="/users">Users</NavLink>
